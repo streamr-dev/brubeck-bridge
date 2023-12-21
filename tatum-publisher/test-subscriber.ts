5496b58c5b63b06b127097179f1e2901d9e9d5a7
Henri Pihkala <henri@streamr.network>
2023-12-21 10:40:51 +0200

    update streams

--- tatum-publisher/test-subscriber.ts
+++ tatum-publisher/test-subscriber.ts
@@ -4,6 +4,7 @@ const streamr = new StreamrClient()
 
 streamr.subscribe({
     streamId: 'eth-watch.eth/ethereum/blocks',
+    //streamId: 'streams.dimo.eth/firehose/weather',
     partition: 0
 }, (msg) => {
     console.log(msg)
