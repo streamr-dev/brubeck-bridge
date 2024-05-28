abb077fd98f4157fb9ace4a29ff6d34e525b2b79
Jarno Marttila <jarno.t.marttila@gmail.com>
2024-05-28 09:33:36 +0200

    upgraded streamr/sdk and related libraries to latest

--- tatum-publisher/test-subscriber.ts
+++ tatum-publisher/test-subscriber.ts
@@ -4,8 +4,8 @@ const streamr = new StreamrClient()
 
 streamr.subscribe({
     // streamId: 'eth-watch.eth/ethereum/blocks',
-    streamId: 'eth-watch.eth/ethereum/events',
-    //streamId: 'streams.dimo.eth/firehose/weather',
+    //streamId: 'eth-watch.eth/ethereum/events',
+    streamId: 'streams.dimo.eth/firehose/weather',
     partition: 0
 }, (msg) => {
     console.log(msg)
